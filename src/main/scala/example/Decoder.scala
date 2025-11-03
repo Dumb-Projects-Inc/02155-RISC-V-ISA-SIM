7c19e6b7a691217745dcf3da58c354a4b0eb318f
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2025-11-03 15:10:44 +0100

    add field decoders

---
+++ src/main/scala/example/Decoder.scala
@@ -0,0 +1,8 @@
+object Fields {
+  def rd(inst: Int): Int = (inst >> 7) & 0x1f
+  def rs1(inst: Int): Int = (inst >> 15) & 0x1f
+  def rs2(inst: Int): Int = (inst >> 20) & 0x1f
+  def funct3(inst: Int): Int = (inst >> 12) & 0x7
+  def funct7(inst: Int): Int = (inst >> 25) & 0x7f
+  def opcode(inst: Int): Int = inst & 0x7f
+}

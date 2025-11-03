649e559d9db2afe0f96f22c3c518e8c7347cb66a
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2025-11-03 16:31:23 +0100

    start work on decoder

--- src/main/scala/rvsim/Decoder.scala
+++ src/main/scala/rvsim/Decoder.scala
@@ -1,5 +1,10 @@
 package rvsim
 
+object Opcode {
+  val R_TYPE = 0b0110011 
+}
+
+
 object Fields {
   def rd(inst: Int): Int = (inst >> 7) & 0x1f
   def rs1(inst: Int): Int = (inst >> 15) & 0x1f
@@ -8,5 +13,30 @@ object Fields {
   def funct7(inst: Int): Int = (inst >> 25) & 0x7f
   def opcode(inst: Int): Int = inst & 0x7f
 
-  def decode(inst: Int): {}
+  def decode(inst: Int): OP = {
+    val opcode = Fields.opcode(inst)
+
+    opcode match {
+      case Opcode.R_TYPE => {
+        val funct7 = Fields.funct7(inst)
+        val funct3 = Fields.funct3(inst)
+        val rs2 = Fields.rs2(inst)
+        val rs1 = Fields.rs1(inst)
+        val rd = Fields.rd(inst)
+
+        funct7 match {
+          case 0b000000 => {
+
+            funct3 match {
+              case 0b001 => {
+                SLLI(rd, rs1, rs2)
+              }
+            }
+          }
+          case 0b0100000 => {}
+        }
+
+      }
+    }
+  }
 }

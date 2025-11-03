758c2535830044fe6bba461b938a45031e480d99
tobias1012 <29657183+tobias1012@users.noreply.github.com>
2025-11-03 15:11:15 +0100

    modified:   src/main/scala/example/vm.scala

--- src/main/scala/example/vm.scala
+++ src/main/scala/example/vm.scala
@@ -6,4 +6,15 @@ class VM() {
   private val mem = new Memory()
   private val regs = new Registers()
   private var pc: UINT_32 = 0x0000_0000
+
+  def step(): Unit = {
+    // Fetch instruction
+    val instr = mem.read(pc)
+    // Decode and execute instruction
+    // TODO: Decoder and extract fields
+  }
+
+  def run(): Unit = {
+    while step() do ()
+  }
 }

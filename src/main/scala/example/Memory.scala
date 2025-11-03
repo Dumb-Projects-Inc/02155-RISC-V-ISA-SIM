5727257f165b4ead189164f31d16f10abd5982bd
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2025-11-03 15:46:20 +0100

    fix memory

--- src/main/scala/example/Memory.scala
+++ src/main/scala/example/Memory.scala
@@ -13,6 +13,7 @@ object Reg extends Enumeration {
   val ZERO, RA, SP, GP, TP, T0, T1, T2, S0, S1, A0, A1, A2, A3, A4, A5, A6, A7,
       S2, S3, S4, S5, S6, S7, S8, S9, S10, S11, T3, T4, T5, T6 = Value
 }
+
 class Registers() {
   private val regs: Array[INT_32] = Array.ofDim[INT_32](32)
 
@@ -22,11 +23,16 @@ class Registers() {
     if (reg != Reg.ZERO) regs(reg.id) = value
   }
 }
-class Memory() {
-  private val memory: Array[INT_32] = Array.ofDim[INT_32](1 << 16)
 
-  def read(addr: INT_32): INT_32 = memory((addr >> 2).toInt)
+class Memory() {
+  private val memory: Array[INT_8] =
+    Array.ofDim[INT_8](1e6) // 1MB of memory allocated
+
+  def read(addr: INT_32): INT_32 = {
+    memory(wordIndex(addr))
+  }
+
   def write(addr: INT_32, value: INT_32): Unit = {
-    memory((addr >> 2).toInt) = value
+    memory(wordIndex(addr)) = value
   }
 }

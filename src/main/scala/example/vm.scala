19cbf85e3ee36fdec8dc7d13d5f5ae25b414eac3
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2025-11-03 14:36:20 +0100

    fix syntax

--- src/main/scala/example/vm.scala
+++ src/main/scala/example/vm.scala
@@ -1,4 +1,5 @@
-class VM:
-    private val mem = Memory()
-    private val regs = Registers()
-    private var pc: UINT_32 = 0x0000_0000
+class VM {
+  private val mem = Memory()
+  private val regs = Registers()
+  private var pc: UINT_32 = 0x0000_0000
+}
